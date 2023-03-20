848bab068b68f45b905b082fc2c593450a054572
Sergey Mezhmal <s.mezhmal@gmail.com>
2023-03-20 23:26:20 +0300

    Best travel (5 kyu): solution 1

---
+++ 5kyu-Best-Travel/src/main/scala/solution.scala
@@ -0,0 +1,23 @@
+import scala.math.pow
+
+case class Example(t: Int, k: Int, ls: List[Int], expected: Int)
+
+@main def solution(params: String*): Unit =
+  val examples: List[Example] = List(
+    Example(174, 3, List(50, 55, 57, 58, 60), 173),
+    Example(163, 3, List(50, 55, 56, 57, 58), 163),
+    Example(163, 3, List(50), -1),
+    Example(163, 1, List(50), 50),
+  )
+  for (example <- examples) {
+    val result = BestTravel.chooseBestSum(example.t, example.k, example.ls)
+    println(s"${if (result == example.expected) '+' else '-'} $result ${if (result == example.expected) '=' else '!'}= ${example.expected}")
+  }
+
+object BestTravel {
+
+    def chooseBestSum(t: Int, k: Int, ls: List[Int]): Int = {
+      val appropriateDistances = ls.combinations(k).map(_.sum).filter(_ <= t)
+      if (appropriateDistances.nonEmpty) appropriateDistances.max else -1
+    }
+}
